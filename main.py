c4f0c46cb501ad64846f980ceba48a94d5a2a64c
Eric Katchr <eric.mathews@katchr.com>
2024-07-28 15:35:42 +0100

    code_reader

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from PyPDF2 import PdfReader
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.agent import ReActAgent
 from prompts import context
+from code_reader import code_reader
 # Initialize the LLM
-llm = Ollama(model="llama2", request_timeout=120)
+llm = Ollama(model="llama2", request_timeout=3600)
 
 # Define a parser function for PDF files
 def parse_pdf(file_path):
@@ -46,10 +47,11 @@ tools = [
                                             description="this gives documentation about code for an API. Use this for reading docs for the API",
                         ),
                     ),
+    code_reader,
 
 ]
 
-code_llm =Ollama(model="codellama", request_timeout=120)
+code_llm =Ollama(model="codellama", request_timeout=3600)
 agent = ReActAgent.from_tools(tools, llm=code_llm, verbose=True, context=context)
 
 while (prompt := input("Enter a prompt (q to quit): ")) != "q":
